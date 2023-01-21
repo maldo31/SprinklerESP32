14fb416b2e2abf0b85a7b6bf47dfd2ce8d25252f
maldo <maldok31@gmail.com>
2023-01-21 21:58:35 +0100

    init

---
+++ SprinklerEndpointEsp32/.pio/build/esp32dev/libd9c/WebServer/detail/mimetable.cpp.d
@@ -0,0 +1,4 @@
+.pio/build/esp32dev/libd9c/WebServer/detail/mimetable.cpp.o: \
+ C:/Users/maldo/.platformio/packages/framework-arduinoespressif32/libraries/WebServer/src/detail/mimetable.cpp \
+ C:/Users/maldo/.platformio/packages/framework-arduinoespressif32/libraries/WebServer/src/detail/mimetable.h \
+ C:/Users/maldo/.platformio/packages/framework-arduinoespressif32/cores/esp32/pgmspace.h
